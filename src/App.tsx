d859ab6aee67035dd00513257989ac51c0be8228
Alan Bruno Ferreira da Silva <alan.ferreira@pixter.com.br>
2023-07-08 16:20:14 -0300

    refactor: add ReduxProvider around App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { Provider as ReduxProvider } from 'react-redux'
+
 import { AddTodo, TodoList } from './components'
+import { store } from './store'
 
 export function App() {
   return (
-    <div>
+    <ReduxProvider store={store}>
       <TodoList />
       <AddTodo />
-    </div>
+    </ReduxProvider>
   )
 }
