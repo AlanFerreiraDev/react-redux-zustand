3bcfae0bd438462f008e04e0f3afe52e15b28506
Alan Bruno Ferreira da Silva <alan.ferreira@pixter.com.br>
2023-07-16 19:09:21 -0300

    feat: create a slice to player

--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -1,7 +1,12 @@
 import { MessageCircle } from 'lucide-react'
 import { Header, Video, Module } from '../components'
+import { useAppSelector } from '../store'
 
 export function Player() {
+  const modules = useAppSelector((state) => {
+    return state.player.course.modules
+  })
+
   return (
     <div className="h-screen bg-zinc-950 text-zinc-50 flex justify-center items-center">
       <div className="flex w-[1100px] flex-col gap-6">
