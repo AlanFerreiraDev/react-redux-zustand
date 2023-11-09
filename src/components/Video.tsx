1d779f69c7ca86401be5a94bb05a917bcc4fec88
Alan Bruno Ferreira da Silva <alan.ferreira@pixter.com.br>
2023-11-09 20:12:29 -0300

    refactor: finish zustand application on app

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,15 +1,18 @@
 import ReactPlayer from 'react-player'
-import { next, useCurrentLesson } from '../store/slices/player'
-import { useAppDispatch, useAppSelector } from '../store'
 import { Loader2 } from 'lucide-react'
+import { useCurrentLesson, useStore } from '../zustand-store'
 
 export function Video() {
-  const dispatch = useAppDispatch()
   const { currentLesson } = useCurrentLesson()
-  const isCourseLoading = useAppSelector((state) => state.player.isLoading)
+  const { next, isLoading } = useStore((store) => {
+    return {
+      next: store.next,
+      isLoading: store.isLoading,
+    }
+  })
 
   function handlePlayNext() {
-    dispatch(next())
+    next()
   }
 
   if (!currentLesson) {
@@ -18,7 +21,7 @@ export function Video() {
 
   return (
     <div className="w-full bg-zinc-950 aspect-video">
-      {isCourseLoading ? (
+      {isLoading ? (
         <div className="flex h-full items-center justify-center">
           <Loader2 className="w-6 h-6 text-zinc-400 animate-spin" />
         </div>
